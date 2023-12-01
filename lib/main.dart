6b1b439531f34b5dfce66c72739b1ac98d372fdf
Renato Deck-Dev <deeckinve@proton.me>
2023-12-01 00:15:31 -0300

    lógica de calculos e validações implementadas ao código

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,49 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+String? _validateField(String value, String fieldName) {
+  if (value.isEmpty) {
+    return 'Por favor, coloque $fieldName';
+  }
+  return null;
+}
+
 class _MyAppState extends State<MyApp> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = 'Infrome seus dados!';
+
+  void _resetFields() {
+    weightController.text = '';
+    heightController.text = '';
+    _infoText = 'Informe seus dados!';
+  }
+
+  void calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      print(imc);
+      if (imc < 18.6) {
+        _infoText = 'Abaixo do Peso (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = 'Peso Ideal (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = 'Levemente Acima do Peso (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = 'Obesidade Grau I (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = 'Obesidade Grau II (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 40) {
+        _infoText = 'Obesidade Grau III (${imc.toStringAsPrecision(4)})';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,7 +71,9 @@ class _MyAppState extends State<MyApp> {
             Padding(
               padding: const EdgeInsets.only(right: 15),
               child: IconButton(
-                onPressed: () {},
+                onPressed: () => setState(() {
+                  _resetFields();
+                }),
                 icon: const Icon(
                   Icons.refresh,
                   color: Colors.white,
@@ -41,73 +85,84 @@ class _MyAppState extends State<MyApp> {
         ),
         body: SingleChildScrollView(
           padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Icon(
-                Icons.person_outline,
-                size: 120,
-                color: Colors.green,
-              ),
-              const TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Peso (kg)',
-                  labelStyle: TextStyle(
-                    color: Colors.green,
-                  ),
-                ),
-                textAlign: TextAlign.center,
-                style: TextStyle(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Icon(
+                  Icons.person_outline,
+                  size: 120,
                   color: Colors.green,
-                  fontSize: 25,
                 ),
-              ),
-              const TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Altura (cm)',
-                  labelStyle: TextStyle(
-                    color: Colors.green,
-                  ),
-                ),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 25,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 10.0,
-                  bottom: 10.0,
-                ),
-                child: SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
+                TextFormField(
+                  controller: weightController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Peso (kg)',
+                    labelStyle: TextStyle(
+                      color: Colors.green,
                     ),
-                    child: const Text(
-                      'Calcular',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
+                  ),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.green,
+                    fontSize: 25,
+                  ),
+                  validator: (value) => _validateField(value!, 'seu peso'),
+                ),
+                TextFormField(
+                  controller: heightController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Altura (cm)',
+                    labelStyle: TextStyle(
+                      color: Colors.green,
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.green,
+                    fontSize: 25,
+                  ),
+                  validator: (value) => _validateField(value!, 'sua altura'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10.0,
+                    bottom: 10.0,
+                  ),
+                  child: SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          calculate();
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                      ),
+                      child: const Text(
+                        'Calcular',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const Text(
-                'Info',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 25,
-                ),
-              )
-            ],
+                Text(
+                  _infoText,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.green,
+                    fontSize: 25,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
